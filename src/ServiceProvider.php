a200f89d98f5de1d2f04d6f3731f371fe752bfa2
Tarek Adam <tarek.adam@gmail.com>
2020-10-02 11:59:12 -0600

    start

---
+++ src/ServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FuquIo\Play;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Class ServiceProvider
+ * @package FuquIo\LaravelCors
+ */
+class ServiceProvider extends BaseServiceProvider{
+	CONST VENDOR_PATH = 'fuqu-io/play';
+	CONST SHORT_NAME = 'fuqu-play';
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot(){
+
+		$this->bootConfig();
+		$this->bootMigrations();
+
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register(){
+
+	}
+
+
+	/**
+	 * @internal
+	 */
+	private function bootConfig(){
+		$this->publishes([__DIR__ . '/../config/main.php' => config_path(SELF::SHORT_NAME . '-client-site.php')], 'config');
+		$this->mergeConfigFrom(__DIR__ . '/../config/main.php', SELF::SHORT_NAME);
+	}
+
+	/**
+	 * @internal
+	 */
+	private function bootMigrations(){
+		$this->loadMigrationsFrom(__DIR__ . '/../migrations');
+	}
+
+}
