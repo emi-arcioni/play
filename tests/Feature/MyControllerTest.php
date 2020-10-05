0927c65679de15bb7a0474c387c81046029c0dcf
Emilio Arcioni <emilio@arcioni.com.ar>
2020-10-05 16:10:58 -0300

    MyControllerTest - test tinkering

---
+++ tests/Feature/MyControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MyControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+
+        $response = $this->get('/test');
+        $response->assertStatus(200);
+        $this->assertArrayHasKey('message', $response);
+
+    }
+}
